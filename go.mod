e5225fba3013ddbd02ace6802488750d01fa9b9c
ArtemKVD <a.kunicza@list.ru>
2025-08-16 13:58:02 +0300

    decode

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.3
 
 require (
 	github.com/gin-gonic/gin v1.10.1
+	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
@@ -31,6 +32,7 @@ require (
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.36.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	"image"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	gozxing "github.com/makiuchi-d/gozxing"
+	decoder "github.com/makiuchi-d/gozxing/qrcode"
 	"github.com/skip2/go-qrcode"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/qr", func(c *gin.Context) {
-		text := c.Query("text")
-
-		code, err := qrcode.Encode(text, qrcode.Medium, 256)
-
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.Data(200, "image/png", code)
-	})
-
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	r.GET("/qr", generateQR)
+	r.POST("/decode", decodeQR)
 
 	r.Run(":8080")
+}
+
+func generateQR(c *gin.Context) {
+	text := c.Query("text")
+	if text == "" {
+		log.Printf("error get text")
+		return
+	}
+
+	png, err := qrcode.Encode(text, qrcode.Medium, 256)
+	if err != nil {
+		log.Printf("error encode")
+		return
+	}
+
+	c.Data(200, "image/png", png)
+}
+
+func decodeQR(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		log.Printf("error get file")
+		return
+	}
+
+	qr, err := file.Open()
+	if err != nil {
+		log.Printf("error open file")
+		return
+	}
+	defer qr.Close()
+
+	img, _, err := image.Decode(qr)
+	if err != nil {
+		log.Printf("decode error")
+		return
+	}
+
+	image, _ := gozxing.NewBinaryBitmapFromImage(img)
+	qrReader := decoder.NewQRCodeReader()
+	result, err := qrReader.Decode(image, nil)
+	if err != nil {
+		log.Printf("error decode qr")
+		return
+	}
+
+	c.JSON(200, gin.H{"text": result.GetText()})
 }
